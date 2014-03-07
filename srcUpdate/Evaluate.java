59e844811ed8c44838e8df9c0b51fd098864745f
Adam Socik <socikadam@gmail.com>
2014-03-06 19:18:50 -0600

    Remove duplicated directories

--- srcUpdate/Evaluate.java
+++
@@ -1,9 +0,0 @@
-/* comparison methods (e.g. isStraightFlush)
- * tie braking methods
- * call evaluate(player p1, player p2)
- * every other one, StarightFlush = 9 = Adam, FourOfAKind = 8 = Emily (evens)
- */
-
-public class Evaluate {
-
-}
